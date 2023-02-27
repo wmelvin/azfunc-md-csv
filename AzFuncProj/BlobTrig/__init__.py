0c21a60095fce2da5e6c16721ba937430cfbfc93
Bill Melvin <bill@billmelvin.com>
2023-02-27 11:52:18 -0500

    Implement blob output.

--- AzFuncProj/BlobTrig/__init__.py
+++ AzFuncProj/BlobTrig/__init__.py
@@ -8,25 +8,30 @@ from pathlib import Path
 from csv_to_md import csv_to_md
 
 
-def main(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
-    
-    #  Only process CSV files.
-    if not myblob.name.lower().endswith(".csv"):
+def main(inblob: func.InputStream, outblob: func.Out[str]):
+    logging.info(f"Blob trigger on '{inblob.name}'")
+
+    if not inblob.name.lower().endswith(".csv"):
+        logging.info("Not a CSV file.")
         return
-    
-    #  Copy blob to temp file.
+
     with tempfile.TemporaryDirectory() as tmpdir_name:
         csv_path = Path(tmpdir_name) / "temp.csv"
         out_path = Path(tmpdir_name) / "temp.md"
-        
-        #  TODO: Copy blob to temp.csv file.
 
-        #  Just make a little test CSV file for now.
-        csv_path.write_text('"A","B","C"\n1,2,3')
+        logging.info(f"Write blob data to temp file '{csv_path}'")
+        with open(csv_path, "wb") as f:
+            f.write(inblob.read())
 
-        csv_to_md.csv_to_md(str(csv_path), out_path, do_info=True, do_source=True)
+        logging.info("Call csv_to_md.")
+        csv_to_md.csv_to_md(
+            str(csv_path),
+            str(out_path),
+            do_info=False,
+            do_source=False
+        )
 
-        #  TODO: Copy temp.md file to blob.
+        logging.info(f"Write '{out_path}' to blob output.")
+        md = out_path.read_text()
+        outblob.set(md)
+
