f87c10a877e9cd2b132c1fb71f6d208516b69844
Bill Melvin <bill@billmelvin.com>
2023-02-27 19:14:01 -0500

    Add to setup script.

--- setup/az-setup.ps1
+++ setup/az-setup.ps1
@@ -1,17 +1,30 @@
+# ----------------------------------------------------------------------
+#  az-setup.ps1
+# ----------------------------------------------------------------------
 
 # az login
+
+
+# ----------------------------------------------------------------------
 
 # -- Create resources for Python Azure Function.
 #    https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-cli-python?pivots=python-mode-configuration&tabs=azure-cli%2Cbash#create-supporting-azure-resources-for-your-function
 
 
-#  Try using the resources created for the FlaskFileUpAz project.
-#  That is a Linux
+#  Try using the resources created for the 'FlaskFileUpAz' project.
+#  This assumes that the 'az-setup-1-storage.ps1' script in that
+#  project has been run and the resources exist. Make sure the
+#  resource names below still match what that script creates.
 
 $rgName = "fileupaz_rg"
 $location = "eastus"
 $storageAcct = "fileupaz01storage"
+
+
+#  Resources for the function app.
+
 $funcAppName = "fileupaz01funcapp"
+
 
 Write-Host "$rgName"
 Write-Host "$location"
@@ -19,7 +32,24 @@ Write-Host "$storageAcct"
 Write-Host "$funcAppName"
 
 
+# -- Get the storage account key.
+#    Example found in Microsoft Docs: "Mount a file share to a Python function app - Azure CLI"
+#    https://docs.microsoft.com/en-us/azure/azure-functions/scripts/functions-cli-mount-files-storage-linux
+
+$storageKey = $(az storage account keys list -g $rgName -n $storageAcct --query '[0].value' -o tsv)
+
+
+# -- Create storage container for function app output.
+#    https://docs.microsoft.com/en-us/cli/azure/storage/container?view=azure-cli-latest#az-storage-container-create
+
+az storage container create `
+    --account-key $storageKey `
+    --account-name $storageAcct `
+    --name "output-md"
+
+
 # -- Create the function app.
+#    https://docs.microsoft.com/en-us/cli/azure/functionapp?view=azure-cli-latest#az-functionapp-create
 
 az functionapp create `
     -g $rgName `
